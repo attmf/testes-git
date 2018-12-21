41a3edfb8d2f2558acd240e7e5a24ffc6088e76a
Anderson Trimont <anderson.maronato@etec.sp.gov.br>
2018-12-21 20:26:07 -0200

    prineiro commit aula 101

---
+++ UdemyPrimeiro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UdemyPrimeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Entre com seu uartos");
+            int uartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com seu preço");
+            decimal preco = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("tudo");
+            string[] dados = Console.ReadLine().Split(' ');
+
+            foreach (string dado in dados)
+            {
+                Console.WriteLine(dado);
+            }
+        }
+    }
+}
